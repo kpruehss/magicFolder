1fd234bc6390afeb05f080402832be0d2074534c
Kartug <Karsten.Pruehss@gmail.com>
2017-01-17 20:09:10 +1100

    testing os module funcationality to find applicable functions

--- magicFolder.py
+++ magicFolder.py
@@ -1,1 +1,11 @@
 import glob
+import os
+
+# get the current directory to run script on
+curDir = os.getcwd()
+
+# testing scandir fucntionality. Will be removed later
+with os.scandir(curDir) as list:
+    for entry in list:
+        if not entry.name.startswith('.') and entry.is_file():
+            print(entry.name)
