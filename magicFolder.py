cb0e413f318567345c8af9a75ce018a4a888d7f5
Kartug <Karsten.Pruehss@gmail.com>
2017-01-19 17:33:08 +1100

    Testing some solutions. NJY

--- magicFolder.py
+++ magicFolder.py
@@ -1,12 +1,18 @@
-import glob
 import os
 
 # get the current directory to run script on
 curDir = os.getcwd()
-
+fileDict = {}
 # testing scandir fucntionality. Will be removed later
 with os.scandir(curDir) as list:
     for entry in list:
         if not entry.name.startswith('.') and entry.is_file():
-            print(entry.path)
-            
+            filelist = (os.path.splitext(entry.name))
+            #print(filelist[1])
+
+            for file in filelist:
+                key = 'Pictures'
+                if file[1] == '.jpeg':
+                   fileDict[key] = file
+                print(fileDict)
+                fileDict.
